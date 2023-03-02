b74775b06bcd044b3c041d4bded10f14f818f869
puzzzik <vtazenkovv@yandex.ru>
2023-03-02 20:56:33 +0300

    created django project

--- storage/client.py
+++ storage/client.py
@@ -1,4 +1,4 @@
-from server import MinioServer
+from storage.server import MinioServer
 
 
 if __name__ == '__main__':
--- storage/server.py
+++ storage/server.py
@@ -19,7 +19,7 @@ class MinioServer:
 
     @staticmethod
     def _get_minio_client():
-        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+        dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
         if os.path.exists(dotenv_path):
             load_dotenv(dotenv_path)
 
