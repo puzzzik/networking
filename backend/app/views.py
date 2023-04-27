05b10215bbceafa536f3036f8846cabc9daa9796
itazenkov <itazenkov@yandex-team.ru>
2023-04-28 00:43:40 +0300

    add setup.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,8 +6,7 @@ from rest_framework.response import Response
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from .serializers import *
 from authentication.models import User
-
-
+from ..grpc_client.client import Client
 @extend_schema(responses=FileSerializer(many=True))
 @api_view(['GET'])
 def list_files(request: Request):
@@ -17,6 +16,7 @@ def list_files(request: Request):
         folder = Folder.objects.get(user__pk=user.pk)
         folder_serializer = FolderSerializer(folder)
         return Response(folder_serializer.data)
+
     except:
         return Response([], status=status.HTTP_404_NOT_FOUND)
 
@@ -29,9 +29,13 @@ def list_files(request: Request):
 )
 @api_view(['GET'])
 def get_file(request: Request):
-    file = File.objects.get(pk=request.file_id)
-    file_serializer = FileSerializer(file)
-    return Response(file_serializer.data)
+    try:
+        file = File.objects.get(pk=request.file_id)
+        info = grpc_client.Client.get_file_info(bucket_name=file.folder.name, file_name=file.name)
+        file_serializer = FileSerializer(name=info.name, last_modified=info.date, hash=info.hash)
+        return Response(file_serializer.data)
+    except:
+        return Response()
 
 
 @extend_schema(request=FileSerializer)
@@ -39,8 +43,6 @@ def get_file(request: Request):
 def post_file_info(request: Request):
     request_user = request.user
     user = User.objects.get(pk=request_user.pk)
-    files = request.FILES
-    print(files)
     return Response()
 
 
@@ -56,10 +58,9 @@ def post_file_info(request: Request):
 )
 @api_view(['POST'])
 def post_file(request: Request):
-    request_user = request.user
-    user = User.objects.get(pk=request_user.pk)
     file: InMemoryUploadedFile = request.FILES['file_field']
     print(file.name)
     print(file.size)
     print(file.content_type)
+    Client.upload_file(bucket_name="test", chunk_data=file.chunks())
     return Response()
--- backend/grpc_client/client.py
+++ backend/grpc_client/client.py
@@ -10,7 +10,10 @@ class Client:
         self.host = 'localhost:50051'
 
     def download_file(self, bucket_name: str, file_name: str):
-        pass
+        with grpc.insecure_channel(self.host) as channel:
+            stub = GreeterStub(channel)
+            file = stub.DownloadFile(file_name=file_name, bucket_name=bucket_name)
+            return file
 
     def upload_file(self, bucket_name: str, file: File, chunk_data):
         with grpc.insecure_channel(self.host) as channel:
@@ -21,7 +24,7 @@ class Client:
                     meta=file_service_pb2.MetaData(
                         filename=file.name,
                         extension=file.extension,
-                        bucket=file.folder.name
+                        bucket=bucket_name
                     )
                 )
             )
@@ -29,7 +32,8 @@ class Client:
     def get_file_info(self, bucket_name: str, file_name: str) -> file_service_pb2.MetaData:
         with grpc.insecure_channel(self.host) as channel:
             stub = GreeterStub(channel)
-            pass
+            meta_data = stub.GetFile(file_name=file_name, bucket_name=bucket_name)
+            return meta_data
 
     def get_file_list(self, bucket_name: str) -> [file_service_pb2.MetaData]:
         with grpc.insecure_channel(self.host) as channel:
@@ -37,55 +41,3 @@ class Client:
             file_list: [file_service_pb2.MetaData] = stub.GetFileList(bucket_name=bucket_name)
             return file_list
 
-
-def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    print("Will try to greet world ...")
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = GreeterStub(channel)
-        # download = stub.DownloadFile(
-        #     file_service_pb2.MetaData(
-        #         bucket='test',
-        #         filename='code',
-        #         extension='txt'
-        #     )
-        # )
-        # print("Greeter client received: " + response.message)
-        # with open('download_response1.txt', 'wb') as f:
-        #     f.write(download.chunk_data)
-
-        # with open('test.txt', 'rb') as f:
-        #     upload = stub.UploadFile(
-        #         file_service_pb2.File(
-        #             chunk_data=f.read(),
-        #             meta=file_service_pb2.MetaData(
-        #                 filename='test',
-        #                 extension='txt',
-        #                 bucket='test'
-        #             )
-        #         )
-        #     )
-        # print(upload)
-
-        # rm = stub.RemoveFile(
-        #     file_service_pb2.MetaData(
-        #         bucket='test',
-        #         filename='test',
-        #         extension='txt'
-        #     )
-        # )
-        # print(rm)
-
-        files = stub.GetFileList(
-            file_service_pb2.FileListRequest(
-                bucket='test'
-            )
-        )
-        print(files.files)
-
-
-if __name__ == '__main__':
-    logging.basicConfig()
-    run()
